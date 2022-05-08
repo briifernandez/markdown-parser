b48951d9236e1395b29dfdf87bdf6e11ead61653
Briana Fernandez <98505287+briifernandez@users.noreply.github.com>
2022-05-07 23:47:02 -0700

    Update

--- MarkdownParseTester.java
+++ MarkdownParseTester.java
@@ -1,4 +1,13 @@
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+
 
 public class MarkdownParseTester{
-    
+
+    @Test
+    public void testMultiply() {
+        assertEquals("result should equal 10", 10,
+                (5 * 2));
+    }
+
 }
